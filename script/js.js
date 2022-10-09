5db49f369ba30ff04f3e62aa73d3733cbebe70b1
yevstrikha <yevstrikha@gmail.om>
2022-10-09 13:29:57 -0400

    Home3

---
+++ script/js.js
@@ -0,0 +1,10 @@
+let a = prompt('Enter first number');
+let b = prompt('Enter second number');
+let c = prompt('Enter third number');
+a = parseInt(a);
+b = parseInt(b);
+c = parseInt(c);
+
+let d = (a+b+c)/3
+
+alert('Your result is ' + d);
